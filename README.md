131f77146a0903256a1f29528995dae50ab78ccb
Marco Gregorin <marcogregorin@aol.com>
2018-09-13 14:22:43 +0200

    Added main setup

--- README.md
+++ README.md
@@ -3,3 +3,17 @@ A Skating game available on web
 
 ## Description
 The skate will be animated as suggested by **Adam Kuhn** on [codepen](https://codepen.io/cobra_winfrey/pen/KQKaRj)
+
+## Tech Specs
+* HTML5
+* CSS3
+* JavaScript ES6
+* NPM
+* Gulp
+* Sass
+* Webpack
+
+## Install Setup
+1. Clone repo
+2. Open terminal
+3. Install dependencies by typing `npm install`
---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+// CREATE A DEFAULT TASK
+gulp.task('default', function(){
+  console.log('this is the default task');
+});
+
+// SASS IT
+gulp.task('styles', function(){
+  return gulp.src('./assets/sass/main.scss')
+    .pipe(autoprefixer())
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest('./dist/css')
+  );
+});
+
+// SET UP WATCH
+gulp.task('watch', function(){
+
+  watch('./assets/sass/**/*.scss', function(){
+    gulp.start('styles');
+  });
+
+});
